e6b4164f59073e33585bf888f73058c78eae1d56
Filippo Garota <filogarota101@gmail.com>
2021-03-04 15:39:30 +0100

    snack 4 completo

--- snack 4 blocco 2/js/script.js
+++ snack 4 blocco 2/js/script.js
@@ -1,13 +1,13 @@
 /* crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari*/
 
 // creo l'array di numeri
-var numeri = [ 20, 2, 12, 10 ];
+var numeri = [ 20, 3, 12, 20, 2, 3 ];
 
 var somma = 0;
 
 for (var i = 0; i < numeri.length; i++) {
-  if (numeri[i] % 2 == 1) {
-    console.log(numeri[i]);
+  if (i % 2 == 1) {
+    somma += numeri[i];
   }
-
 }
+console.log( 'la somma dei numeri dispari è' + ' ' + somma);
