648c21193c040144e944654c70d5259fd2768938
Filippo Garota <filogarota101@gmail.com>
2021-03-04 12:16:52 +0100

    Snack 10

---
+++ snack-10/js/script.js
@@ -0,0 +1,17 @@
+/*Inserisci un numero, se è pari stampa il numero, se è dispari
+stampa il numero successivo*/
+
+
+var numero = parseInt(prompt('Inserisci un numero'));
+
+if (isNaN(numero)) {
+  console.log('Attento! Inserisci un numero');
+}
+
+if (numero % 2 == 0) {
+  console.log(numero);
+}
+
+if (numero % 2 == 1) {
+  console.log(numero % 2 == 0);
+}
