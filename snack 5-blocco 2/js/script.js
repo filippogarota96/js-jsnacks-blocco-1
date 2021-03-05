d9d357deb0d74dedb4dfaa2643f5e7ff09c6e7c4
Filippo Garota <filogarota101@gmail.com>
2021-03-05 10:02:07 +0100

    snack 5 aggiunto nuovo while

--- snack 5-blocco 2/js/script.js
+++ snack 5-blocco 2/js/script.js
@@ -5,11 +5,17 @@
   /*creo i due array*/
 
   var firstArray = [3, 5, 6, 5];
-  var secondArray = [12, 43, 16, 67, 32];
+  var secondArray = [12, 43, 16, 67, 32, 30, 54, 12];
 
   while (firstArray.length < secondArray.length) {
 
     firstArray.push(Math.floor(Math.random() * 10));
+    firstArray.length == secondArray.length;
+  }
+
+  while (secondArray.length < firstArray.length) {
+
+    secondArray.push(Math.floor(Math.random() * 10));
     firstArray.length == secondArray.length;
   }
 
